994eb43af69c49de8e6b7aa4b876a1521ad9f150
Kosama1334 <75474064+Kosama1334@users.noreply.github.com>
2025-11-27 00:13:13 +0530

    Getting started

--- local_state/main.tf
+++ local_state/main.tf
@@ -1,1 +1,25 @@
+# Terraform Block
+terraform {
+  required_version = ">= 1.2"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.92"
+    }
+  }
+}
 
+#Provider Block
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Resource blocks
+resource "aws_instance" "tf_first_instance" {
+  ami = "ami-0fa3fe0fa7920f68e"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "TF_first_instance"
+  }
+}
